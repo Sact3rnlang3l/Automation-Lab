22d783ac8010681520c0745adba2dfa2aa17e5ce
Aiden gillespie <aiden@cold.org>
2021-08-25 14:59:21 -0600

    Cleaned up syntax

--- movieList/index.test.js
+++ movieList/index.test.js
@@ -1,7 +1,6 @@
-const { Builder, Capabilities, By } = require("selenium-webdriver")
+const { Builder, Capabilities, By } = require("selenium-webdriver");
 
 const chromedriver = require("chromedriver");
-
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
@@ -20,23 +19,24 @@ test("Add The Worst Horror Movie Ever to the Web List", async () => {
     By.xpath("/html/body/main/section/form/button")
   );
   await add_movie.sendKeys("Wrestling Women VS the Aztec Mummy");
-  await click_button.click()
+  await click_button.click();
   await driver.sleep(2000);
   // await add_movie.sendKeys("Wrestling Women VS the Aztec Mummy");
-  // await click_button.click()  
+  // await click_button.click()
   // await add_movie.sendKeys("Wrestling Women VS the Aztec Mummy");
   // await click_button.click()
-  
 });
- test('Cross off Worst Horror Movie Ever', async() =>{
+test("Cross off Worst Horror Movie Ever", async () => {
   //  (By.xpath('(/html/body/main/ul/li)[1]'))
-   let cross_movie = await driver.findElement(By.xpath('/html/body/main/ul/li'))
-   await cross_movie.click()
+  let cross_movie = await driver.findElement(By.xpath("/html/body/main/ul/li"));
+  await cross_movie.click();
   await driver.sleep(1000);
- })
-test('Purge the Worst', async()=>{
+});
+test("Purge the Worst", async () => {
   // (By.xpath('//ul/li/button')
-  let purge = await driver.findElement(By.xpath('//*[@id="WrestlingWomenVStheAztecMummy"]'))
-  await purge.click()
-  await driver.sleep(2000)
-})
+  let purge = await driver.findElement(
+    By.xpath('//*[@id="WrestlingWomenVStheAztecMummy"]')
+  );
+  await purge.click();
+  await driver.sleep(2000);
+});
